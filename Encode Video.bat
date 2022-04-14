f320e20f15fbe7b7f989951719c31328ba02f697
CondeSaheki <48620486+CondeSaheki@users.noreply.github.com>
2022-04-14 17:22:31 -0400

    Update and rename Encode video.bat to Encode Video.bat

--- Encode Video.bat
+++ Encode Video.bat
@@ -2,10 +2,19 @@
 setlocal
 SET "name=%~dpn1"
 pause
+ECHO -----
+ECHO lembre-se 
+ECHO 5mb + 10mb/min
+ECHO incluindo audio hitsounds storyboads background
+ECHO -----
+ECHO Olhe essa calculadora caso nao saiba quais valores tentar
+ECHO https://www.dr-lex.be/info-stuff/videocalc.html
+ECHO -----
 ECHO Insira o average bitrate:
 SET /P average_a=
 ECHO Insira o bitrate para bufsize ( 1x a 2x o valor do average bitrate):
 SET /P bufsize_a=
+ECHO -----
 pause
 ffmpeg -hide_banner -y -i %* -an -c:v libx264 -s 1280x720 -r 23.976 -preset veryslow -b:v %average_a%k -maxrate %bufsize_a%k -tune fastdecode -profile:v high -bufsize %bufsize_a%k -pix_fmt yuv420p -map 0:0 -map_metadata -1 -pass 1 -f flv NUL
 ffmpeg -hide_banner -i %* -an -c:v libx264 -s 1280x720 -r 23.976 -preset veryslow -b:v %average_a%k -maxrate %bufsize_a%k -tune fastdecode -profile:v high -bufsize %bufsize_a%k -pix_fmt yuv420p -map 0:0 -map_metadata -1 -pass 2 "%name% video %average_a%k %bufsize_a%k.flv"
