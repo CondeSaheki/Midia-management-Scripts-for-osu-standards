18963ea3e36bdcbcc37839ed05df8d09aa1939d1
CondeSaheki <48620486+CondeSaheki@users.noreply.github.com>
2022-04-14 17:03:20 -0400

    Update Encode in mp3 192k.bat

--- Encode in mp3 192k.bat
+++ Encode in mp3 192k.bat
@@ -5,9 +5,9 @@ ECHO %*
 SET "name=%~dpn1"
 ECHO "%name%"
 pause
-ffmpeg -i %* -filter:a volumedetect -f null /dev/null
-ECHO Insira o maxvolume:
+ffmpeg -i %* -vn -filter:a volumedetect -f null /dev/null
+ECHO Insira o inverso de maxvolume:
 SET /P volume=
 
-ffmpeg -i %* -filter:a "volume=%volume%dB" -vn -c:a libmp3lame -ar 44100 -ac 2 -b:a 192k -joint_stereo 0 -map_metadata -1 "%name% output 192.mp3"
+ffmpeg -hide_banner -i %* -filter:a "volume=%volume%dB" -vn -c:a libmp3lame -ar 44100 -ac 2 -b:a 192k -joint_stereo 0 -map_metadata -1 "%name% 192k.mp3"
 PAUSE
